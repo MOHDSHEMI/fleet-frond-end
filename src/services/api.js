8dd035de532d4edd72a3974939e2674164bc8aa9
MOHDSHEMEEM <Muhammed.Shemeem@ompventure.com>
2026-07-06 05:32:13 +0530

    chore(api): switch baseURL to production endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://fleet-back-end.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
